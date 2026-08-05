0bea7862db999690a216c59769f16cbe72bef3c7
VidyaRichard <158169586+VidyaRichard@users.noreply.github.com>
2026-08-05 16:48:41 +0530

    Add files via upload

---
+++ day1_assgn_main.js
@@ -0,0 +1,31 @@
+var firstName="sam";
+var companyName="abc pvt ltd";
+var isAutomation=true;
+var mobileNumber=12345689;
+var hasPlaywright;
+
+console.log("firstname-------",firstName,typeof(firstName));
+console.log("comapny name-----",companyName,typeof(companyName));
+console.log("mobilenumber-----",mobileNumber,typeof(mobileNumber));
+console.log("is automation-----",isAutomation,typeof(isAutomation));
+console.log("hasPlaywright-------",typeof(hasPlaywright));
+
+
+
+var comapnyname="kk";
+var companyname="ww";
+companyname="world";
+console.log("company name",companyname)
+
+let fName="john";
+//let fName="july";
+fName="priya";
+console.log("firstName",fName);
+
+const lName="doe";
+console.log("LastName",lName);
+let y ;
+console.log(y);
+
+
+
---
+++ day2_func.js
@@ -0,0 +1,15 @@
+const  browserVersion="chrome";
+
+function getBrowserVersion()
+{
+
+if(browserVersion==="chrome")
+{
+
+let browserversion="safari";
+ console.log("local variable", browserversion);
+
+}
+console.log("Global variable ",  browserVersion);}
+
+getBrowserVersion();
