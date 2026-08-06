b12418d5c65af02f47c03d6e9813e9bb9e3e83d7
VidyaRichard <158169586+VidyaRichard@users.noreply.github.com>
2026-08-06 09:30:13 +0530

    Add files via upload
    Week1

---
+++ 02_Number.js
@@ -0,0 +1,21 @@
+let  number ;
+number=-0;
+
+function checkNumber(number)
+{
+if(number>0)
+{
+    console.log("Given number is a positive nmber")
+
+}
+else if(number<0)
+{
+    console.log("Given number is a negative nmber0");
+
+}
+else 
+{
+    console.log("number is zero");
+}
+}
+checkNumber(number);
