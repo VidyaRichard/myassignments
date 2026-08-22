59ef55375fa5e06e8387314bbc12fc0783f325d5
VidyaRichard <158169586+VidyaRichard@users.noreply.github.com>
2026-08-22 17:04:44 +0530

    Add files via upload
    week3

---
+++ week3_Pg8_day2_editLead.spec.ts
@@ -0,0 +1,44 @@
+import { test } from '@playwright/test';
+// locate the elements correctly while testing some changes had made by vidya 
+//this is creating lead 
+test('launch browser', async ({ page }) => {
+  await page.goto('https://login.salesforce.com');
+await page.locator(('[name="username"]')).fill('dilipkumar.rajendran@testleaf.com');
+await page.locator('input[id="Login"]').click()
+await page.locator('#password').fill('TestLeaf@2025');
+await page.locator('input[id="Login"]').click()
+await page.waitForLoadState('domcontentloaded')
+
+await page.locator('//div[@class="slds-icon-waffle"]').click()
+await page.locator('[aria-label="View All Applications"]').click();
+await page.waitForLoadState('domcontentloaded')
+
+//await page.getByText('Sales', { exact: true }).click();
+await page.locator('[data-name="Sales"]').nth(0).click()
+await page.waitForTimeout(1000)
+
+const link1=page.locator('a[href="/lightning/o/Lead/home"]')
+link1.click()
+//creating lead
+/*
+await page.locator('//a[@class="forceActionLink"]').nth(0).click();
+await  page.locator('[aria-label="Salutation"]').click()
+await page.getByPlaceholder('First Name').fill("vidya");
+await page.locator('//input[@class="slds-input"]').nth(4).fill("abc company")
+await page.locator('[name="SaveEdit"]').click()
+await page.waitForTimeout(1000)
+*/
+
+
+await page.getByPlaceholder('Search this list...').fill("riya richard")
+await page.locator('.slds-checkbox_faux').first().click();
+const element = page.getByTitle('Show more actions').first();
+await element.click();
+//await page.getByRole('menuitem', { name: 'Edit', exact: true }).click();
+await page.waitForTimeout(7000)
+
+
+
+  
+});
+
---
+++ week3_Pg9_leftapp_editlead.spec.ts
@@ -0,0 +1,39 @@
+import { test } from '@playwright/test';
+//Dropdown sample program 
+test('launch browser', async ({ page }) => {
+  await page.goto("http://leaftaps.com/opentaps/control/main")
+  await page.locator('input[name="USERNAME"]').fill("democsr2");
+  await page.locator('input[name="PASSWORD"]').fill("crmsfa");
+  await page.locator('input[type=submit]').click();
+  await page.locator('a[href*="crmsfa"]').nth(0).click();
+  await page.locator('a[href*="lead"]').nth(0).click();
+  //creating lead 
+  await page.locator('a[href*="create"]').nth(0).click();
+  await page.locator('input[name="companyName"]').fill("VR")
+  await page.locator('input[name*="firstName"]').nth(2).fill('vidya');
+  await page.locator('input[name="lastName"]').nth(2).fill("richard")
+  await page.locator('input[name="personalTitle"]').fill("sa")
+  await page.locator('input[name="generalProfTitle"]').fill("software")
+  await page.locator('input[name="annualRevenue"]').fill("20000")
+  await page.locator('input[name="departmentName"]').fill("IT")
+  await page.locator('select[name="dataSourceId"]').selectOption({ label: 'Cold Call' });
+  const dropdown=await page.locator('select[name="dataSourceId]"]')
+  const options =dropdown.locator('option')
+  const count =options.count();
+  for(let i=0;i<await count;i++)
+  {
+    const option=options.nth(i)
+    console.log(option);
+    console.log("Lead created successfully")
+
+//editing lead 
+    await page.locator('a[href="/crmsfa/control/viewLead?partyId=10015"]').click();
+    await page.locator('a[href="updateLeadForm?partyId=10015"]').click();
+    await page.waitForTimeout(7000)
+    await page.locator('#updateLeadForm_firstName').clear();
+    await page.locator('#updateLeadForm_firstName').fill("vidya");
+    await page.locator('//input[@class="smallSubmit"]').nth(0).click()
+
+
+
+});
